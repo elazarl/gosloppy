5e1a003505b1dce0ff44b8dd6464a67d89c921a0
Leibovich, Elazar <elazarl@gmail.com>
2013-02-28 20:26:23 +0200

    failed test for importing subpackages and test utils

--- instrument/instrument_test.go
+++ instrument/instrument_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sort"
 	"testing"
 )
 
@@ -26,13 +27,93 @@ type Fs struct {
 	Children []*Fs
 }
 
+type FsList []*Fs
+
+func (l FsList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l FsList) Less(i, j int) bool {
+	return l[i].Name < l[j].Name
+}
+
+func (l FsList) Len() int {
+	return len(l)
+}
+
+func (fs *Fs) IsDir() bool {
+	return fs.Children != nil
+}
+
+func (fs *Fs) List() (children []*Fs) {
+	children = append(children, fs.Children...)
+	sort.Sort(FsList(children))
+	return
+}
+
+func (fs *Fs) AssertEqual(path string, t *testing.T) {
+	info, err := os.Stat(path)
+	orFail(err, t)
+	fs.recursiveEqual(filepath.Dir(path), info, t)
+}
+
+func (fs *Fs) String() string {
+	name := fs.Name
+	if fs.IsDir() {
+		name += "(d)"
+	}
+	return name
+}
+
+func fileinfos(infos []os.FileInfo) string {
+	b := make([]byte, 0, 100)
+	for _, info := range infos {
+		b = append(b, " "+info.Name()...)
+		if info.IsDir() {
+			b = append(b, "(d)"...)
+		}
+	}
+	return string(b)
+}
+
+// Compare returns whether a certain *Fs node is equal to a file
+// NOT recursive.
+func (fs *Fs) recursiveEqual(path string, info os.FileInfo, t *testing.T) {
+	path = filepath.Join(path, info.Name())
+	if fs.Name != info.Name() {
+		t.Fatal(path, "expected", fs.Name)
+	}
+	if fs.IsDir() != info.IsDir() {
+		t.Fatal(path, "isDir=", info.IsDir(), "expected", fs.IsDir())
+	}
+	if fs.IsDir() {
+		children, err := ioutil.ReadDir(path)
+		orFail(err, t)
+		if len(children) != len(fs.Children) {
+			t.Fatal("expected", fs.Children, "got", fileinfos(children))
+		}
+		for i, child := range fs.List() {
+			if child.Name != children[i].Name() {
+				t.Fatal("expected", fs.Children, "got", fileinfos(children))
+			}
+			child.recursiveEqual(path, children[i], t)
+		}
+	} else {
+		content, err := ioutil.ReadFile(path)
+		orFail(err, t)
+		if fs.Content != string(content) {
+			t.Fatalf(path, "expected content", fs.Content, "got", string(content))
+		}
+	}
+}
+
 func (fs *Fs) Remove(path string) error {
 	return os.RemoveAll(filepath.Join(path, fs.Name))
 }
 
 func (fs *Fs) Build(path string) error {
 	name := filepath.Join(path, fs.Name)
-	if fs.Children == nil {
+	if !fs.IsDir() {
 		if err := ioutil.WriteFile(name, []byte(fs.Content), 0644); err != nil {
 			return err
 		}
@@ -61,7 +142,7 @@ func assertFileIs(filename, content string, t *testing.T) {
 	b, err := ioutil.ReadFile(filename)
 	orFail(err, t)
 	if string(b) != content {
-		t.Error("Expected", content, "got", string(b))
+		t.Error("File", filename, "Expected", content, "Actually", string(b))
 	}
 }
 
@@ -78,10 +159,38 @@ func TestDir(t *testing.T) {
 		t.Fatal("Expected [a.go a_test.go] got", dir.Files())
 	}
 	orFail(os.Mkdir("temp", 0755), t)
+	defer func() { orFail(os.RemoveAll("temp"), t) }()
 	orFail(dir.Instrument("temp", func(pf *patch.PatchableFile) patch.Patches {
 		return patch.Patches{patch.Replace(pf.File, "koko")}
 	}), t)
 	assertFileIs("temp/a.go", "koko", t)
 	assertFileIs("temp/a_test.go", "koko", t)
+}
+
+func TestSubDir(t *testing.T) {
+	fs := dir(
+		"test",
+		dir("sub1", file("sub1.go", "package sub1")),
+		dir("sub2", file("sub2.go", "package sub2")),
+		dir("sub3", file("sub3.go", `package sub3;import "../sub1"`)),
+		file("base.go", `package test1;import "./sub1"`), file("a_test.go", `package test1;import "./sub2"`),
+	)
+	orFail(fs.Build("."), t)
+	defer func() { orFail(fs.Remove("."), t) }()
+	pkg, err := ImportDir("test", "test")
+	orFail(err, t)
+	if fmt.Sprint(pkg.Files()) != "[test/base.go test/a_test.go]" {
+		t.Fatal("Expected [test/base.go test/a_test.go] got", pkg.Files())
+	}
+	orFail(os.Mkdir("temp", 0755), t)
+	orFail(pkg.Instrument("temp", func(pf *patch.PatchableFile) patch.Patches {
+		return patch.Patches{patch.Replace(pf.File, "koko")}
+	}), t)
 	defer func() { orFail(os.RemoveAll("temp"), t) }()
+	dir("temp",
+		dir("sub1", file("sub1.go", "koko")),
+		dir("sub2", file("sub2.go", "koko")),
+		file("a.go", "koko"),
+		file("a_test.go", "koko"),
+	).AssertEqual("temp", t)
 }
