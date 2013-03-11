7843587ade0a6b7da3a41dcf901fe3c9e44bcecc
Elazar Leibovich <elazarl@gmail.com>
2013-03-12 00:00:20 +0200

    MultiVisitor now really works...

--- multivisitor.go
+++ multivisitor.go
@@ -7,42 +7,39 @@ import (
 type MultiVisitor []ScopeVisitor
 
 func (v MultiVisitor) VisitExpr(scope *ast.Scope, expr ast.Expr) ScopeVisitor {
-	any := false
-	for i, w := range v {
-		if w != nil {
-			v[i] = w.VisitExpr(scope, expr)
-			any = true
+	allnil := true
+	for _, w := range v {
+		if w.VisitExpr(scope, expr) != nil {
+			allnil = false
 		}
 	}
-	if !any {
+	if allnil == true {
 		return nil
 	}
 	return v
 }
 
 func (v MultiVisitor) VisitStmt(scope *ast.Scope, stmt ast.Stmt) ScopeVisitor {
-	any := false
-	for i, w := range v {
-		if w != nil {
-			v[i] = w.VisitStmt(scope, stmt)
-			any = true
+	allnil := true
+	for _, w := range v {
+		if w.VisitStmt(scope, stmt) != nil {
+			allnil = false
 		}
 	}
-	if !any {
+	if allnil == true {
 		return nil
 	}
 	return v
 }
 
 func (v MultiVisitor) ExitScope(scope *ast.Scope, node ast.Node, last bool) ScopeVisitor {
-	any := false
-	for i, w := range v {
-		if w != nil {
-			v[i] = w.ExitScope(scope, node, last)
-			any = true
+	allnil := true
+	for _, w := range v {
+		if w.ExitScope(scope, node, last) != nil {
+			allnil = false
 		}
 	}
-	if !any {
+	if allnil == true {
 		return nil
 	}
 	return v
