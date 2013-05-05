8814d8d5f26b4d03ac9b9fbb2ebfebd3aa8255a3
Leibovich, Elazar <elazarl@gmail.com>
2013-05-05 17:41:49 +0300

    most of the packages work, probably must define new goroot while instrumenting...

--- test/disabled_testgoroot/run.sh
+++ test/disabled_testgoroot/run.sh
@@ -1,4 +1,4 @@
-find "$GOROOT/src/pkg" -type d| while read d; do
+find "$GOROOT/src/pkg" -type d -not -name reflect | grep -vw runtime | grep -v '\(net\|time\|syscall\|sync\|testing\)' | while read d; do
 	find "$d" -name \*_test.go -type f -maxdepth 1|while read f; do
 		(cd $d; $GOSLOPPY test) || (echo error while building $d; exit -1)
 		break
